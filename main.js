e16ee3de02f7b53ed5268f53dba178e05ee78274
gianbaeza <gianb04@gmail.com>
2023-03-28 23:17:43 -0300

    agregue fuction

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
+//Alumno: Gian Franco Baeza
+
 let cantidadVentas = 0;
 let gananciasTotales = 0;
 
+function registrarVenta(){
+    let cantidadCafes = prompt("Ingrese la cantidad de cafés vendidos:");
+    let precioCafe = prompt("Ingrese el precio unitario del café:");
+
+    cantidadVentas++;
+    gananciasTotales += cantidadCafes * precioCafe;
+
+    return gananciasTotales;
+}
+
 while(true){
-let opcion = prompt("Bienvenido al sistema de registro de ventas de café. ¿Qué acción desea realizar?\n1. Registrar una venta\n2. Ver ganancias totales\n3. Salir");
+  let opcion = prompt("Bienvenido al sistema de registro de ventas de café. ¿Qué acción desea realizar?\n1. Registrar una venta\n2. Ver ganancias totales\n3. Salir");
 
-if(opcion == "1"){
-let cantidadCafes = prompt("Ingrese la cantidad de cafés vendidos:");
-let precioCafe = prompt("Ingrese el precio unitario del café:");
+  if(opcion == "1"){
+      gananciasTotales = registrarVenta();
 
-cantidadVentas++;
-gananciasTotales += cantidadCafes * precioCafe;
-
-alert(`Venta registrada. Ganancias totales hasta ahora: ${gananciasTotales}`);
-}
-
-else if(opcion == "2"){
-alert(`Las ganancias totales hasta ahora son:  ${gananciasTotales}`);
-}
-
-else if(opcion == "3"){
-alert("¡Hasta la próxima!")
-break;
-}
-
-else{
-alert(" Opción inválida. Por favor seleccione una opción válida. ");
-}
+      alert(`Venta registrada. Ganancias totales hasta ahora: ${gananciasTotales}`);
+  }else if(opcion == "2"){
+      alert(`Las ganancias totales hasta ahora son:  ${gananciasTotales}`);
+  }else if(opcion == "3"){
+      alert("¡Hasta la próxima!")
+      break;
+  }else{
+      alert(" Opción inválida. Por favor seleccione una opción válida. ");
+  }
 }
